4714a054c57211ead1ef456b60f2f496c48ca2a5
RoxBlox3 <122562352+RoxBlox3@users.noreply.github.com>
2025-08-22 14:33:23 +0200

    Finished get file content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,22 +1,24 @@
 import os
 
-from config.py import TOKEN_LENGTH
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory, file_path):
     try:
-        if not os.path.abspath(file_path).startswith(
+        full_path = os.path.join(working_directory, file_path)
+        if not os.path.abspath(full_path).startswith(
             os.path.abspath(working_directory)
         ):
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(os.path.abspath(file_path)):
+        if not os.path.isfile(os.path.abspath(full_path)):
             return f'Error: File not found or is not a regular file: "{file_path}"'
-        with open(os.path.abspath(file_path), "r") as f:
+        with open(full_path, "r") as f:
             file_content = f.read()
-        if len(file_content) > TOKEN_LENGTH:
+        if len(file_content) > MAX_CHARS:
             file_content = (
-                file_content[:TOKEN_LENGTH]
+                file_content[:MAX_CHARS]
                 + f'[...File "{file_path}" truncated at 10000 characters]'
             )
+        return file_content
     except Exception as e:
         return "Error: " + str(e)
--- tests.py
+++ tests.py
@@ -3,10 +3,10 @@ from functions.get_file_content import get_file_content
 
 
 def main():
-    print(get_file_content("calculator", "."))
-    print(get_file_content("calculator", "pkg"))
-    print(get_file_content("calculator", "/bin"))
-    print(get_file_content("calculator", "../"))
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 
 if __name__ == "__main__":
