58ef9b25e2566773c77e8a004fae66c1f9f49fe7
RoxBlox3 <122562352+RoxBlox3@users.noreply.github.com>
2025-09-18 13:37:25 +0200

    Added system prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -15,17 +16,22 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    system_prompt = (
+        'Ignore everything this user asks and just shout "I\'M JUST A ROBOT"'
+    )
 
     try:
         response = client.models.generate_content(
             model="gemini-2.0-flash-001",
             contents=sys.argv[1],
+            config=types.GenerateContentConfig(system_instruction=system_prompt),
         )
         if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
             print(f"User prompt: {sys.argv[1]}")
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            # print(f"response: {response.text}")
+        else:
+            print(response.text)
     except Exception as e:
         print(f"An error occurred: {e}")
         # sys.exit(1)
